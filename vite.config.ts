4dfd582d62bbef279290abbc542f37e0ad3a014c
Brady <brady.savarie@hotmail.com>
2023-08-08 23:57:45 -0400

    install mui and styled-components

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@mui/styled-engine': resolve(
+        __dirname,
+        'node_modules/@mui/styled-engine-sc'
+      ),
+    },
+  },
+  base: '/',
   plugins: [react()],
-})
+});
