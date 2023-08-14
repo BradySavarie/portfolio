c4b16db4b7aef4c8052823ff392663e5eb38953c
Brady <brady.savarie@hotmail.com>
2023-08-14 00:05:33 -0400

    create router and NotFound component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,4 @@
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
@@ -10,25 +9,23 @@ import { Stack } from '@mui/material';
 
 export default function Navbar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {'<BRADY__SAVARIE />'}
-          </Typography>
-          <Stack direction="row" spacing={2}>
-            <Button color="inherit">Projects</Button>
-            <Button color="inherit">About</Button>
-            <Button color="inherit">Skills</Button>
-            <Button color="inherit">Contact</Button>
-          </Stack>
-          <IconButton size="large" edge="start">
-            <LightModeIcon />
-            <DarkModeIcon />
-          </IconButton>
-          <Button color="inherit">Download Resume</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          {'<BRADY__SAVARIE />'}
+        </Typography>
+        <Stack direction="row" spacing={2}>
+          <Button color="inherit">Projects</Button>
+          <Button color="inherit">About</Button>
+          <Button color="inherit">Skills</Button>
+          <Button color="inherit">Contact</Button>
+        </Stack>
+        <IconButton size="large" edge="start">
+          <LightModeIcon />
+          <DarkModeIcon />
+        </IconButton>
+        <Button color="inherit">Download Resume</Button>
+      </Toolbar>
+    </AppBar>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
 import App from './App';
+import NotFound from './components/NotFound';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
