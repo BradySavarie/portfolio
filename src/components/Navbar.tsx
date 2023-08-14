146624a1ef36deb4f2acbd4173bf7c133741e53f
Brady <brady.savarie@hotmail.com>
2023-08-13 23:25:19 -0400

    Restructure navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,26 +4,29 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import { Stack } from '@mui/material';
 
 export default function Navbar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            {'<BRADY__SAVARIE />'}
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Stack direction="row" spacing={2}>
+            <Button color="inherit">Projects</Button>
+            <Button color="inherit">About</Button>
+            <Button color="inherit">Skills</Button>
+            <Button color="inherit">Contact</Button>
+          </Stack>
+          <IconButton size="large" edge="start">
+            <LightModeIcon />
+            <DarkModeIcon />
+          </IconButton>
+          <Button color="inherit">Download Resume</Button>
         </Toolbar>
       </AppBar>
     </Box>
