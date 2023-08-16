1d25d9d766b359e15c781f39410ac5f6ec406522
Brady <brady.savarie@hotmail.com>
2023-08-15 23:35:28 -0400

    create useColorTheme hook

---
+++ src/theme/use-color-theme.ts
@@ -0,0 +1,19 @@
+import { createTheme, PaletteMode } from '@mui/material';
+import React from 'react';
+import { getDesignTokens } from './theme';
+
+const useColorTheme = () => {
+  const [mode, setMode] = React.useState<PaletteMode>('light');
+
+  const toggleColorMode = () =>
+    setMode((prev) => (prev === 'light' ? 'dark' : 'light'));
+
+  const modifiedTheme = React.useMemo(
+    () => createTheme(getDesignTokens(mode)),
+    [mode]
+  );
+
+  return { theme: modifiedTheme, mode, toggleColorMode };
+};
+
+export default useColorTheme;
