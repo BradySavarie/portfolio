d5a22df74687b6df491b81fa7543c0ffef8017c2
Brady <brady.savarie@hotmail.com>
2023-08-16 00:08:08 -0400

    create theme context provider

---
+++ src/theme/ThemeContextProvider.tsx
@@ -0,0 +1,26 @@
+import { createTheme, Theme } from '@mui/material';
+import { createContext, PropsWithChildren, useContext } from 'react';
+import useColorTheme from './use-color-theme';
+
+type ThemeContextType = {
+  theme: Theme;
+  mode: string;
+  toggleColorMode: () => void;
+};
+
+export const ThemeContext = createContext<ThemeContextType>({
+  theme: createTheme(),
+  mode: 'light',
+  toggleColorMode: () => {},
+});
+
+export function ThemeContextProvider({ children }: PropsWithChildren) {
+  const value = useColorTheme();
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
+}
+
+export const useThemeContext = () => {
+  return useContext(ThemeContext);
+};
