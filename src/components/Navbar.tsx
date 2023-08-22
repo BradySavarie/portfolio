b544e1f31c4c2277873da457f4859ffef94fd444
Brady <brady.savarie@hotmail.com>
2023-08-21 23:00:12 -0400

    Style mobile drawer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,9 +8,11 @@ import {
   IconButton,
   Drawer,
   useMediaQuery,
+  Divider,
 } from '@mui/material';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
+import CloseIcon from '@mui/icons-material/Close';
 import MenuIcon from '@mui/icons-material/Menu';
 import { NavLink } from 'react-router-dom';
 import { useState } from 'react';
@@ -83,7 +85,9 @@ export default function Navbar() {
               <Stack
                 direction="row"
                 spacing={2}
-                sx={{ height: theme.spacing(4) }}
+                sx={{
+                  height: theme.spacing(4),
+                }}
               >
                 <IconButton size="large" edge="start" onClick={toggleColorMode}>
                   {mode === 'light' ? (
@@ -107,11 +111,14 @@ export default function Navbar() {
               </Stack>
             </Stack>
             {isSmallScreen ? null : (
-              <IconButton size="large" onClick={toggleDrawer}>
+              <IconButton
+                size="large"
+                onClick={toggleDrawer}
+                sx={{ marginLeft: theme.spacing(1) }}
+              >
                 <MenuIcon
                   sx={{
                     color: theme.palette.text.primary,
-                    marginLeft: theme.spacing(1),
                   }}
                 />
               </IconButton>
@@ -119,12 +126,144 @@ export default function Navbar() {
           </Toolbar>
         </Container>
       </AppBar>
-      <Drawer anchor="right" open={isOpen} onClose={toggleDrawer}>
+
+      <Drawer
+        anchor="right"
+        open={isOpen}
+        onClose={toggleDrawer}
+        sx={{ bgcolor: theme.palette.secondary.main }}
+      >
         <Container
           sx={{
             minWidth: '80vw',
           }}
-        />
+        >
+          <Stack
+            direction="row"
+            spacing={2}
+            sx={{ paddingY: theme.spacing(1) }}
+          >
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{
+                color: theme.palette.text.primary,
+                fontWeight: 600,
+                flexGrow: 1,
+                whiteSpace: 'nowrap',
+                alignSelf: 'center',
+              }}
+            >
+              {'<BRADY__SAVARIE />'}
+            </Typography>
+            <IconButton size="large" onClick={toggleDrawer}>
+              <CloseIcon />
+            </IconButton>
+          </Stack>
+        </Container>
+        <Divider role="presentation" variant="fullWidth" />
+        <Container
+          sx={{
+            paddingY: theme.spacing(2),
+          }}
+        >
+          <Stack
+            direction="column"
+            spacing={1}
+            sx={{
+              alignItems: 'left',
+            }}
+          >
+            <NavLink to="/">
+              <Button
+                sx={{
+                  color: theme.palette.text.secondary,
+                }}
+              >
+                Projects
+              </Button>
+            </NavLink>
+            <NavLink to="/">
+              <Button
+                sx={{
+                  color: theme.palette.text.secondary,
+                }}
+              >
+                About
+              </Button>
+            </NavLink>
+            <NavLink to="/">
+              <Button
+                sx={{
+                  color: theme.palette.text.secondary,
+                }}
+              >
+                Skills
+              </Button>
+            </NavLink>
+            <NavLink to="/">
+              <Button
+                sx={{
+                  color: theme.palette.text.secondary,
+                }}
+              >
+                Contacts
+              </Button>
+            </NavLink>
+          </Stack>
+        </Container>
+        <Divider role="presentation" variant="fullWidth" />
+        <Container>
+          <Stack
+            direction="row"
+            spacing={2}
+            sx={{
+              paddingY: theme.spacing(1),
+              marginX: theme.spacing(1),
+            }}
+          >
+            <Typography
+              variant="body1"
+              component="div"
+              sx={{
+                color: theme.palette.text.primary,
+                flexGrow: 1,
+                whiteSpace: 'nowrap',
+                alignSelf: 'center',
+              }}
+            >
+              Switch Color Mode
+            </Typography>
+            <IconButton size="large" edge="start" onClick={toggleColorMode}>
+              {mode === 'light' ? (
+                <LightModeIcon sx={{ color: theme.palette.text.primary }} />
+              ) : (
+                <DarkModeIcon sx={{ color: theme.palette.text.primary }} />
+              )}
+            </IconButton>
+          </Stack>
+          <Container
+            sx={{
+              paddingY: theme.spacing(1),
+              marginX: theme.spacing(1),
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            <Button
+              sx={{
+                bgcolor: theme.palette.text.primary,
+                color: theme.palette.secondary.main,
+                borderRadius: theme.shape.borderRadius,
+                px: theme.spacing(2),
+                py: theme.spacing(1),
+                whiteSpace: 'nowrap',
+              }}
+            >
+              Download Resume
+            </Button>
+          </Container>
+        </Container>
       </Drawer>
     </>
   );
