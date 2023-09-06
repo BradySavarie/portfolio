081c5968deebf82b63330d21296e999cb72b0897
Brady <brady.savarie@hotmail.com>
2023-09-05 22:12:33 -0400

    mend

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { resolve } from 'path';
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable @typescript-eslint/naming-convention */
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(__fileName);
 
 // https://vitejs.dev/config/
 export default defineConfig({
